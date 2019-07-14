21df26bf0c432810db501d54c0d64e2c62dd970e
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-07-14 10:53:09 +0200

    initial comit

---
+++ src/main/java/com/example/fotomfs/FotomfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.fotomfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotomfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FotomfsApplication.class, args);
+    }
+
+}
