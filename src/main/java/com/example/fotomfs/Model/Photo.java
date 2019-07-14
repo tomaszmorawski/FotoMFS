0f688c90e853c1ec8707dbc4db7305dcb423ffad
Tomasz Mórawski <tomasz.morawski1987@gmail.com>
2019-07-14 11:24:23 +0200

    Added models for Photo, Role, User

---
+++ src/main/java/com/example/fotomfs/Model/Photo.java
@@ -0,0 +1,20 @@
+package com.example.fotomfs.Model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Photo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private boolean chosen = false;
+}
---
+++ src/main/java/com/example/fotomfs/Model/Role.java
@@ -0,0 +1,19 @@
+package com.example.fotomfs.Model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
